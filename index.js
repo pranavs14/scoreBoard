75226237a3fe7cad41ecf4b067ba35c435e9c4b3
Pranav Shyam <61221640+pranavs14@users.noreply.github.com>
2025-02-27 21:56:28 +0530

    Update index.js{2}

--- index.js
+++ index.js
@@ -15,11 +15,3 @@ function guestScore(num){
     guest += num;
     guestScoreElem.textContent = guest;
 }
-
-document.querySelectorAll('.score-increment button').forEach(button => {
-    button.addEventListener('click', () => {
-        const isHome = button.parentElement.previousElementSibling.id === "home-score";
-        const increment = parseInt(button.textContent);
-        isHome ? homeScore(increment) : guestScore(increment);
-    });
-});
